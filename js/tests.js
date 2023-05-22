cea96a766108896e1014db530d31fcf362a4df40
Destinee Sanders <destineees@gmail.com>
2023-05-22 11:13:31 -0700

    added function for problem two to pass

--- js/tests.js
+++ js/tests.js
@@ -12,6 +12,7 @@ function testSum() { //eslint-disable-line
 }
 
 function testMultiply() { //eslint-disable-line
+  // eslint-disable-next-line no-undef
   if (multiply(5,9)[0] === 45 && multiply(5,9)[1] === 'The product of 5 and 9 is 45.') {
     console.log('%c TEST FOR multiply() PASSES', 'color: green');
   } else {
